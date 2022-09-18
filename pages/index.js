c6b78b4e9ab58e72ac2ffd638e7fdf8be5c003d2
SomayaAbdulkareem <90135545+MhdGmal1998@users.noreply.github.com>
2022-09-18 15:51:40 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ githubProfileData }) {
 			<Proficiency />
 			<Education />
 			<Experience />
-			<Feedbacks />
+			{/* <Feedbacks /> */}
 			<Projects />
 			<GithubProfileCard prof={githubProfileData} />
 		</div>
